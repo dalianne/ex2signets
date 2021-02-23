a48021c1313102c5589514589eeb76ebb8aa5497
dalianne <1850269@cmaisonneuve.qc.ca>
2021-02-23 15:35:04 -0500

    fini!

--- src/composants/BtnAjoutRessource.jsx
+++ src/composants/BtnAjoutRessource.jsx
@@ -1,7 +1,13 @@
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import "./BtnAjoutRessource.scss"
+
 export default function BtnAjoutRessource() {
-  return (
-    <button className="BtnAjoutRessource">
-      Ajouter
-    </button>
-  );
+    return (
+        <button className="BtnAjoutRessource">
+            <Fab color="secondary" aria-label="add">
+                <AddIcon />
+            </Fab>    
+        </button>
+    );
 }
--- src/composants/Entete.jsx
+++ src/composants/Entete.jsx
@@ -1,10 +1,14 @@
+import "./Entete.scss"
+import Avatar from '@material-ui/core/Avatar';
 import avatar from '../images/avatar.png';
 
 export default function Entete() {
   return (
     <header className="Entete">
       <div className="logo">Signets</div>
-      <div className="avatar">Camille Semaan <img src={avatar} alt="Camille Semaan" /></div>
+      <div className="avatar">
+        Dalianne G. <Avatar alt="Dalianne G." src={avatar} />
+      </div>
     </header>
   );
 }
